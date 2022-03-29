d5b95235ae9e32a0c8dfa7aa016ddc3c5123e625
KhashayarB <kbehnam@web.de>
2022-03-29 16:06:04 +0200

    HeapFinish

--- assets/script/main.js
+++ assets/script/main.js
@@ -112,4 +112,93 @@ quickSort.addEventListener("click", () => {
   console.timeEnd("Quick Sort");
 });
 
-heapSort.addEventListener("click", () => {});
+heapSort.addEventListener("click", () => {
+  const heapSortArray = createArray();
+  console.log("Unsorted:", heapSortArray);
+
+  console.log("Sorted: ", heapSort(heapSortArray));
+
+  console.time("Heap Sort");
+
+  function buildMaxHeap(arr) {
+    // Get index of the middle element
+    let i = Math.floor(arr.length / 2 - 1);
+
+    // Build a max heap out of
+    // All array elements passed in
+    while (i >= 0) {
+      heapify(arr, i, arr.length);
+      i -= 1;
+    }
+  }
+
+  function heapify(heap, i, max) {
+    let index;
+    let leftChild;
+    let rightChild;
+
+    while (i < max) {
+      index = i;
+
+      // Get the left child index
+      // Using the known formula
+      leftChild = 2 * i + 1;
+
+      // Get the right child index
+      // Using the known formula
+      rightChild = leftChild + 1;
+
+      // If the left child is not last element
+      // And its value is bigger
+      if (leftChild < max && heap[leftChild] > heap[index]) {
+        index = leftChild;
+      }
+
+      // If the right child is not last element
+      // And its value is bigger
+      if (rightChild < max && heap[rightChild] > heap[index]) {
+        index = rightChild;
+      }
+
+      // If none of the above conditions is true
+      // Just return
+      if (index === i) {
+        return;
+      }
+
+      // Else swap elements
+      swap(heap, i, index);
+
+      // Continue by using the swapped index
+      i = index;
+    }
+  }
+
+  function swap(arr, firstItemIndex, lastItemIndex) {
+    const temp = arr[firstItemIndex];
+
+    // Swap first and last items in the array
+    arr[firstItemIndex] = arr[lastItemIndex];
+    arr[lastItemIndex] = temp;
+  }
+
+  function heapSort(arr) {
+    // Build max heap
+    buildMaxHeap(arr);
+
+    // Get the index of the last element
+    lastElement = arr.length - 1;
+
+    // Continue heap sorting until we have
+    // One element left
+    while (lastElement > 0) {
+      swap(arr, 0, lastElement);
+      heapify(arr, 0, lastElement);
+      lastElement -= 1;
+    }
+
+    // Return sorted array
+    return arr;
+  }
+  console.timeEnd("Heap Sort");
+});
